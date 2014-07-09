a0d82a2d71356d368e87193c8d86c751f01ab2e1
Yoshua Wakeham <yoshwakeham@gmail.com>
2014-07-09 13:42:55 +1000

    added docstrings, comments

--- ulamspiral.py
+++ ulamspiral.py
@@ -2,7 +2,7 @@
 #
 # ::: ULAMSPIRAL :::
 #
-# AUTHOR: Yoshua Wakeham
+# AUTHORS
 #
 #    Yoshua Wakeham
 #        email: yoshwakeham@gmail.com
@@ -45,6 +45,10 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+# CHANGE LOG
+#
+#    9 Jul 14: added docstrings, comments
+#
 ###############################################################################
 
 import sys
@@ -72,12 +76,26 @@ EAST =  (         0,          1)
 ### FUNCTIONS #################################################################
 
 def spiral(image, lower, upper, primes):
+    """
+    spiral(image, int, int, list) -> None
+
+    Takes an image (a rectangular list of lists), lower
+    and upper bounds (integers), and a list of the prime
+    numbers (assumed to be) in the range 1 - upper.
+
+    Modifies the input image by traversing it according
+    to the pattern of Ulam's spiral, starting in the centre
+    of the image and moving out in CCW fashion. Each pixel
+    is assigned an integer, beginning with the lower bound.
+    If the integer is prime, the pixel is painted; otherwise
+    it is left unchanged.
+    """
+    # get image dimensions
     height = len(image)
     if height == 0:
         width = 0
     else:
         width = len(image[0])
-
     # make a new two-dimensional list, equal in size
     # to the image, to record which pixels the spiral
     # has already visited
@@ -86,11 +104,12 @@ def spiral(image, lower, upper, primes):
     row, col = (height/2, width/2)
     orientn = NORTH
     # paint initial cell (and record that the spiral
-    # was here, then move to second cell
+    # was here), then move to second cell
     paintcell(image, row, col, WHITE)
     trail[row][col] = 1
     row, col = advance(row, col, orientn)
-
+    # proceed along the spiral until upper
+    # bound is reached
     for n in range(lower+1, upper+1):
         if prm.is_sieved_prime(n, primes):
             paintcell(image, row, col, BLUE)
@@ -102,22 +121,59 @@ def spiral(image, lower, upper, primes):
 
 
 def paintcell(image, row, col, colour):
+    """
+    paintcell(image, int, int, colour) -> None
+
+    Changes the colour of the pixel at the specified
+    row and column of the input image. The image must
+    be a rectangular list of lists, with parameter 'colour'
+    having the same format as the pixels of the image.
+    """
     image[row][col] = colour
 
 
 def advance(row, col, orientn):
+    """
+    advance(int, int, orientation) -> new coordinates (int, int)
+
+    Moves the spiral one cell 'forward'.
+
+    The orientation is assumed to be a 2-tuple
+    storing offset information for traversing an image
+    stored as a rectangular list of lists. This accounts
+    for the fact that 'advancing' mutates co-ordinates in
+    different ways depending on orientation.
+
+    Returns a 2-tuple with the updated row/col co-ordinates.
+    """
     rowoffset = orientn[0]
     coloffset = orientn[1]
     return row + rowoffset, col + coloffset
 
 
 def turnleft(orientn):
+    """
+    turnleft(orientation) -> new orientation
+
+    Returns the orientation which results from a 'left turn'.
+    Assumes that orientations have been predefined.
+    """
     dirs = [NORTH, WEST, SOUTH, EAST]
     index = (dirs.index(orientn) + 1) % 4
     return dirs[index]
 
 
 def getleft(row, col, orientn):
+    """
+    getleft(int, int, orientation) -> cell co-ordinates (int, int)
+
+    Get row/col co-ordinates of cell to the left of current
+    location. The orientation is assumed to be a 2-tuple
+    storing offset information for traversing an image
+    stored as a rectangular list of lists. This accounts
+    for the fact that 'left' is relative to orientation.
+    Returns a 2-tuple with the co-ords of the required cell.
+    """
     if orientn in [NORTH, SOUTH]:
         rowoffset = orientn[1]
         coloffset = orientn[0]
@@ -140,6 +196,7 @@ if __name__ == '__main__':
         lower = int(sys.argv[1])
         upper = int(sys.argv[2])
     else:
+        # only one bound supplied
         lower = 1
         upper = int(sys.argv[1])
 
@@ -147,8 +204,11 @@ if __name__ == '__main__':
         print("error: lower bound must be strictly lower than upper bound")
         exit()
 
+    # generate a list of primes from 1 to upper
     primes = prm.prime_sieve(upper)
+    # total number of pixels required
     num_cells = upper - lower + 1
+    # define image size accordingly
     width = height = int(sqrt(num_cells)) + 1
 
     image = [[BLACK]*width for _i in range(height)]
